f89102ad469ba31bd209f8d302dcc02d9b69e7d6
Susan Pan <susan.pan96@gmail.com>
2020-10-01 09:34:21 +1000

    updated add, update and display

--- rate-movie.rb
+++ rate-movie.rb
@@ -17,25 +17,39 @@ choice = gets.chomp
 #Case statement for input 'choice'
 case choice
 when "add"
-	#Prompt title & rating
 	print "Title: "
 	title = gets.chomp
-	print "Rating: "
-	rating = gets.chomp
 
-	#Add to movie hash table, convert to symbol & int
-	movies[title.to_sym] = rating.to_i
-
-	movies.each do |title, rating|
-		puts "#{title}: #{rating}\n"
+	#If movie doesn't already exist, prompt for rating
+	if movies[title.to_sym].nil?
+		print "Rating: "
+		rating = gets.chomp
+		movies[title.to_sym] = rating.to_i
+		puts "Added!"
+	else 
+		puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
 	end
 
-	puts "Added!"
-
 when "update"
-	puts "update"
+	#Prompt for title, if exists update rating
+	print "Title: "
+	title = gets.chomp
+	if movies[title.to_sym].nil?
+		#If title, doesn't exist, redirect to add new movie.
+		puts "That movie doesn't exist. Add new movie."
+	else 
+		print "New rating: "
+		new_rate = gets.chomp
+		movies[title.to_sym] = new_rate
+		puts "Updated!"	
+	end
+
 when "display"
-	puts "Movies!"
+	#Print each key=>value pair in hash table
+	movies.each { |title, rating|
+		puts "#{title}: #{rating}"
+	}
+
 when "delete"
 	puts "Deleted!"
 else 
