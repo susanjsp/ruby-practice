a0f5f6b12818d1850d6f50dc55c562eeaa903599
Susan Pan <susan.pan96@gmail.com>
2020-10-01 09:00:41 +1000

    flesh out added section

--- rate-movie.rb
+++ rate-movie.rb
@@ -11,18 +11,33 @@ movies = {
 }
 
 #Prompt user for input
+puts "What would you like to do?"
 choice = gets.chomp
 
 #Case statement for input 'choice'
 case choice
-	when "add"
-		puts "Added!"
-	when "update"
-		puts "update"
-	when "display"
-		puts "Movies!"
-	when "delete"
-		puts "Deleted!"
-	else 
-		puts "Error!"
+when "add"
+	#Prompt title & rating
+	print "Title: "
+	title = gets.chomp
+	print "Rating: "
+	rating = gets.chomp
+
+	#Add to movie hash table, convert to symbol & int
+	movies[title.to_sym] = rating.to_i
+
+	movies.each do |title, rating|
+		puts "#{title}: #{rating}\n"
+	end
+
+	puts "Added!"
+
+when "update"
+	puts "update"
+when "display"
+	puts "Movies!"
+when "delete"
+	puts "Deleted!"
+else 
+	puts "Error!"
 end
